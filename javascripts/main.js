167facb1c0cbf0e6f43899227cf980bf1613d0f0
Haziman Hashim <hazimanhashim@abh.my>
2016-03-21 12:59:54 +0800

    update layout

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+$(function(){
+	
+	var app = angular.module("jimanx2", ['selectize']);
+	app.controller("ContentCtrl", function(){
+		
+		this.myModel = 1;
+		this.myOptions = [
+			{id: 1, title: 'Blog'},
+			{id: 2, title: 'Ruby on Rails'},
+			{id: 3, title: 'Perl'},
+			{id: 4, title: 'Python'},
+		];
+		
+		this.myConfig = {
+			create: true,
+			valueField: 'id',
+			labelField: 'title',
+			sortField: 'title',
+			placeholder: 'Pick something',
+			maxItems: 1
+		};
+	});
+})
